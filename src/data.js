4906c120b6805de54d1b41e52c060e5fe05b252d
Dallin <Dallin Kooyman>
2022-04-14 02:16:02 +0000

    Update for CP3

---
+++ src/data.js
@@ -0,0 +1,702 @@
+var data = [
+	{
+		id: "0BF6BCED-D2F2-6175-2683-ED4E2BBD1D36",
+		first_name: "Adria",
+		last_name: "Nichols",
+		wins: "313",
+		loses: "358"
+	},
+	{
+		id: "E4B2A210-8389-A4F1-E3A5-CE65F7B9B88E",
+		first_name: "Dolan",
+		last_name: "Byers",
+		wins: "185",
+		loses: "655"
+	},
+	{
+		id: "76449A60-A53D-893C-15E7-3827430C7D35",
+		first_name: "Vincent",
+		last_name: "Pollard",
+		wins: "953",
+		loses: "143"
+	},
+	{
+		id: "1005193C-8169-917F-7EAA-CE58E22A0155",
+		first_name: "Tiger",
+		last_name: "Everett",
+		wins: "351",
+		loses: "23"
+	},
+	{
+		id: "16DDE431-EDC3-C373-3BAA-579CC1278702",
+		first_name: "Bernard",
+		last_name: "Cochran",
+		wins: "195",
+		loses: "545"
+	},
+	{
+		id: "91B24A7C-848C-F12A-B8DD-7E7657863EC3",
+		first_name: "Dalton",
+		last_name: "Reeves",
+		wins: "179",
+		loses: "966"
+	},
+	{
+		id: "D216E551-B7B5-B923-F87B-7B8A95557F16",
+		first_name: "Rashad",
+		last_name: "Palmer",
+		wins: "514",
+		loses: "239"
+	},
+	{
+		id: "66DFEB64-4697-FAA8-8F47-2703E11DD6A4",
+		first_name: "Camilla",
+		last_name: "Meyer",
+		wins: "757",
+		loses: "301"
+	},
+	{
+		id: "3FAEC3EF-2B40-24E6-740E-F3A9BB7BC5BD",
+		first_name: "Valentine",
+		last_name: "Duran",
+		wins: "785",
+		loses: "244"
+	},
+	{
+		id: "A402DA26-DA35-96A4-7714-824CCD74EA8C",
+		first_name: "Dawn",
+		last_name: "Massey",
+		wins: "922",
+		loses: "88"
+	},
+	{
+		id: "4B5E2348-BA3D-483D-3113-ACF3159B7B65",
+		first_name: "Rooney",
+		last_name: "Ellis",
+		wins: "688",
+		loses: "96"
+	},
+	{
+		id: "A7D275C6-5E87-6E8E-A4BA-C11344F7668C",
+		first_name: "Wynter",
+		last_name: "Morton",
+		wins: "243",
+		loses: "442"
+	},
+	{
+		id: "E7095B63-7E91-4164-9AA7-A4C65DE126F1",
+		first_name: "Cody",
+		last_name: "Mcdonald",
+		wins: "199",
+		loses: "566"
+	},
+	{
+		id: "AE190B02-ADDE-326C-603A-72D8D3666B03",
+		first_name: "Shad",
+		last_name: "Summers",
+		wins: "98",
+		loses: "535"
+	},
+	{
+		id: "64871EED-4DB0-D4F7-6ABA-28DB615ED53B",
+		first_name: "Charles",
+		last_name: "Whitfield",
+		wins: "819",
+		loses: "663"
+	},
+	{
+		id: "497A3554-F38E-8178-0BBB-25B31781A566",
+		first_name: "Tallulah",
+		last_name: "David",
+		wins: "846",
+		loses: "207"
+	},
+	{
+		id: "E30FAF89-182A-8C0E-0562-B6CBD1BA62B4",
+		first_name: "Ray",
+		last_name: "Howard",
+		wins: "914",
+		loses: "326"
+	},
+	{
+		id: "D9D095D3-B42A-894B-C456-6AE57EFC1661",
+		first_name: "Nayda",
+		last_name: "Daniel",
+		wins: "792",
+		loses: "826"
+	},
+	{
+		id: "848651D7-02E4-D1D1-4E64-EF9BBC462AB7",
+		first_name: "Dominic",
+		last_name: "Wong",
+		wins: "102",
+		loses: "960"
+	},
+	{
+		id: "A07D5CE9-DE3D-E44B-21E6-2E9783861E83",
+		first_name: "Zachery",
+		last_name: "Becker",
+		wins: "626",
+		loses: "146"
+	},
+	{
+		id: "9F4A3999-D661-18BD-D1B0-2EE60552CE39",
+		first_name: "Dacey",
+		last_name: "Morrow",
+		wins: "360",
+		loses: "415"
+	},
+	{
+		id: "20579054-E2D8-008A-87CB-515379AF27A0",
+		first_name: "Xandra",
+		last_name: "Reyes",
+		wins: "523",
+		loses: "147"
+	},
+	{
+		id: "A1DA4524-AEBC-4229-BFB9-3F148E2B36EB",
+		first_name: "Sylvester",
+		last_name: "Harrell",
+		wins: "541",
+		loses: "280"
+	},
+	{
+		id: "64575452-CBAE-3489-93B4-7C4B54CE8FAA",
+		first_name: "Tamara",
+		last_name: "Avery",
+		wins: "46",
+		loses: "89"
+	},
+	{
+		id: "2B5E8353-495E-961B-C915-46E4931AA935",
+		first_name: "Gregory",
+		last_name: "Blevins",
+		wins: "510",
+		loses: "654"
+	},
+	{
+		id: "8DDAA890-6571-8735-AE96-732E55065295",
+		first_name: "Cruz",
+		last_name: "Owen",
+		wins: "554",
+		loses: "405"
+	},
+	{
+		id: "2C5A4AE6-9EB7-9578-B800-7780A5D31957",
+		first_name: "Joseph",
+		last_name: "Jordan",
+		wins: "801",
+		loses: "81"
+	},
+	{
+		id: "416C8E40-5DAA-79BA-DB9D-A70BCC6ED55D",
+		first_name: "Kelly",
+		last_name: "Rojas",
+		wins: "840",
+		loses: "768"
+	},
+	{
+		id: "43EDD3E1-229B-2955-ABB5-4D95B6DA5E2E",
+		first_name: "Dennis",
+		last_name: "Strickland",
+		wins: "232",
+		loses: "358"
+	},
+	{
+		id: "B8D709AD-094D-E5E4-6E3C-B55B16C8ACC8",
+		first_name: "Cameron",
+		last_name: "Roach",
+		wins: "914",
+		loses: "452"
+	},
+	{
+		id: "A752146E-91DE-7DC8-C15E-CEC52C5B42E7",
+		first_name: "Carl",
+		last_name: "Turner",
+		wins: "190",
+		loses: "703"
+	},
+	{
+		id: "D9915A1E-C215-DC59-AC06-9933C622B1C0",
+		first_name: "Iliana",
+		last_name: "Massey",
+		wins: "517",
+		loses: "511"
+	},
+	{
+		id: "5D6A18E4-B8B8-B383-BA37-A40AE7221758",
+		first_name: "Karen",
+		last_name: "Frank",
+		wins: "781",
+		loses: "131"
+	},
+	{
+		id: "84CF238A-59EA-D053-C948-385BD6DCE73F",
+		first_name: "Logan",
+		last_name: "Waters",
+		wins: "746",
+		loses: "746"
+	},
+	{
+		id: "F2264E2E-6476-02F5-C791-F8CD25C54F3E",
+		first_name: "Ori",
+		last_name: "Johnson",
+		wins: "514",
+		loses: "959"
+	},
+	{
+		id: "33BDDC6D-C4ED-C571-DBCD-D622ED131633",
+		first_name: "Francesca",
+		last_name: "Roberts",
+		wins: "295",
+		loses: "281"
+	},
+	{
+		id: "89CECDED-0C1C-B85A-4FBB-0B4ADD54CEA1",
+		first_name: "Jordan",
+		last_name: "Valenzuela",
+		wins: "151",
+		loses: "592"
+	},
+	{
+		id: "738CF82E-3666-EB45-88D6-5299CE7CCED2",
+		first_name: "Rhea",
+		last_name: "Gaines",
+		wins: "306",
+		loses: "963"
+	},
+	{
+		id: "58517DA0-92FE-5DAF-D966-57F8DBC4CEAE",
+		first_name: "Geraldine",
+		last_name: "English",
+		wins: "357",
+		loses: "328"
+	},
+	{
+		id: "22197A51-2D68-3781-D916-216B6C372A5B",
+		first_name: "Judah",
+		last_name: "O'Neill",
+		wins: "211",
+		loses: "233"
+	},
+	{
+		id: "611E6E05-D92B-AEC1-3A64-B8E46304AC2E",
+		first_name: "Roanna",
+		last_name: "Zamora",
+		wins: "28",
+		loses: "19"
+	},
+	{
+		id: "CE64F7D5-9F32-8095-D5C6-38E07B58CD83",
+		first_name: "Ariana",
+		last_name: "Ramos",
+		wins: "750",
+		loses: "485"
+	},
+	{
+		id: "9F7BD894-3F58-B3BE-5613-27D420A8E1B1",
+		first_name: "Anthony",
+		last_name: "Mcleod",
+		wins: "947",
+		loses: "457"
+	},
+	{
+		id: "B3B21C6E-D559-37D7-5A9E-26FCDDDB40B4",
+		first_name: "Whitney",
+		last_name: "Fletcher",
+		wins: "951",
+		loses: "66"
+	},
+	{
+		id: "948C2826-4B2A-F43C-E9D2-2B8E795392FF",
+		first_name: "Tanek",
+		last_name: "Thomas",
+		wins: "275",
+		loses: "937"
+	},
+	{
+		id: "D55A9E86-159A-D2A1-ABA3-B8A24671232B",
+		first_name: "Bruno",
+		last_name: "Sims",
+		wins: "520",
+		loses: "368"
+	},
+	{
+		id: "5563E987-FBD9-1741-171D-B6D914999C1F",
+		first_name: "Buckminster",
+		last_name: "Armstrong",
+		wins: "784",
+		loses: "417"
+	},
+	{
+		id: "F5BB9278-E26B-2B1E-5C7E-4DD2B8891444",
+		first_name: "Hakeem",
+		last_name: "Peck",
+		wins: "965",
+		loses: "547"
+	},
+	{
+		id: "27BEE85C-6C56-D11D-EB49-1CC47BBD12FB",
+		first_name: "Emmanuel",
+		last_name: "Phillips",
+		wins: "652",
+		loses: "966"
+	},
+	{
+		id: "31DD9938-84B1-AC8F-D475-22DCE9C583B8",
+		first_name: "Kasimir",
+		last_name: "Baldwin",
+		wins: "184",
+		loses: "850"
+	},
+	{
+		id: "95CB19B8-E8A9-FDD7-19AB-828C730D61FD",
+		first_name: "Rahim",
+		last_name: "Clements",
+		wins: "639",
+		loses: "269"
+	},
+	{
+		id: "4D3552D4-75FD-779B-4936-227C10573E33",
+		first_name: "Martin",
+		last_name: "Maynard",
+		wins: "395",
+		loses: "963"
+	},
+	{
+		id: "BD112CEC-0824-1BA6-B825-DC044AD7D10B",
+		first_name: "Nehru",
+		last_name: "Castillo",
+		wins: "474",
+		loses: "100"
+	},
+	{
+		id: "6DE7C446-6737-12CB-8833-F94C87F5AA38",
+		first_name: "Emerson",
+		last_name: "Harmon",
+		wins: "179",
+		loses: "471"
+	},
+	{
+		id: "B9BD82CD-3324-2398-1815-AA2244C76F51",
+		first_name: "Galvin",
+		last_name: "Terrell",
+		wins: "561",
+		loses: "648"
+	},
+	{
+		id: "9B1518E8-3BC9-6D39-E71D-16DD38E4EE5D",
+		first_name: "Emerald",
+		last_name: "Neal",
+		wins: "456",
+		loses: "685"
+	},
+	{
+		id: "B5447E8B-8943-AD19-2051-45B731DD4A81",
+		first_name: "Erasmus",
+		last_name: "Summers",
+		wins: "816",
+		loses: "553"
+	},
+	{
+		id: "7352C5CE-4545-F821-B30A-1492054497C9",
+		first_name: "Rana",
+		last_name: "Stone",
+		wins: "378",
+		loses: "337"
+	},
+	{
+		id: "848AA11A-3667-DD4B-CDFE-D23880D61F76",
+		first_name: "Sawyer",
+		last_name: "Puckett",
+		wins: "858",
+		loses: "615"
+	},
+	{
+		id: "EECE7424-29FC-5A5D-D849-43356646D567",
+		first_name: "Reese",
+		last_name: "Hurley",
+		wins: "198",
+		loses: "248"
+	},
+	{
+		id: "4C25672C-DDA8-074A-52C4-6500CCBC9671",
+		first_name: "Vernon",
+		last_name: "Day",
+		wins: "801",
+		loses: "46"
+	},
+	{
+		id: "D8E2636C-BEC2-FC72-83BD-1ECACD8A6333",
+		first_name: "Dexter",
+		last_name: "Wong",
+		wins: "206",
+		loses: "182"
+	},
+	{
+		id: "284D214A-DC94-D184-2181-7C45655BEC4E",
+		first_name: "Joan",
+		last_name: "Arnold",
+		wins: "714",
+		loses: "861"
+	},
+	{
+		id: "52BDD0C4-D91B-4D66-D2C1-8A36461D2786",
+		first_name: "Indigo",
+		last_name: "Giles",
+		wins: "53",
+		loses: "149"
+	},
+	{
+		id: "EAE9B3BE-813A-61B5-8625-9992CE6214A9",
+		first_name: "Briar",
+		last_name: "Parks",
+		wins: "54",
+		loses: "483"
+	},
+	{
+		id: "478F5FAB-454A-A8AA-C67E-E60142EDEAAF",
+		first_name: "Yolanda",
+		last_name: "Mcdowell",
+		wins: "906",
+		loses: "914"
+	},
+	{
+		id: "703AE474-B5D8-82A7-E984-88B8F514CA93",
+		first_name: "Melanie",
+		last_name: "Wiggins",
+		wins: "54",
+		loses: "4"
+	},
+	{
+		id: "F5CCE609-4C84-8FD3-3387-48D6D687ED39",
+		first_name: "Ingrid",
+		last_name: "Riddle",
+		wins: "134",
+		loses: "791"
+	},
+	{
+		id: "CA84F64E-CD63-C822-237D-81E469ADD3A6",
+		first_name: "Castor",
+		last_name: "Leonard",
+		wins: "679",
+		loses: "247"
+	},
+	{
+		id: "8EB9EAAB-BDE4-7E8A-1998-E8E7771A5BA4",
+		first_name: "Martha",
+		last_name: "Morton",
+		wins: "79",
+		loses: "35"
+	},
+	{
+		id: "EBBC6E0D-D469-46C5-C87D-3BE7C06F6304",
+		first_name: "Kelly",
+		last_name: "Bender",
+		wins: "387",
+		loses: "23"
+	},
+	{
+		id: "C8248580-C989-A220-72C6-7C032AD3C25B",
+		first_name: "Nola",
+		last_name: "Fields",
+		wins: "102",
+		loses: "472"
+	},
+	{
+		id: "257651F5-067D-7873-4663-CCA1C57252D6",
+		first_name: "Chantale",
+		last_name: "Potter",
+		wins: "64",
+		loses: "777"
+	},
+	{
+		id: "DEB89836-23D6-B33D-81BE-6738191E3342",
+		first_name: "Alexandra",
+		last_name: "Estes",
+		wins: "782",
+		loses: "124"
+	},
+	{
+		id: "3E6D6C18-8185-8F49-8344-9048D09AD197",
+		first_name: "Thor",
+		last_name: "Porter",
+		wins: "237",
+		loses: "860"
+	},
+	{
+		id: "D7457D0C-758B-2E22-06E3-7C8E9B4D6914",
+		first_name: "Nell",
+		last_name: "Callahan",
+		wins: "750",
+		loses: "378"
+	},
+	{
+		id: "74C99AA8-37D8-5146-7D09-5BB5794DA11B",
+		first_name: "Cameron",
+		last_name: "Owen",
+		wins: "209",
+		loses: "656"
+	},
+	{
+		id: "92E2D7CC-D501-1EEE-92D7-EE46CCFF2D07",
+		first_name: "Austin",
+		last_name: "Fletcher",
+		wins: "392",
+		loses: "648"
+	},
+	{
+		id: "465EA741-D882-F710-BFA8-C2ACD939252B",
+		first_name: "Kasimir",
+		last_name: "Shepherd",
+		wins: "976",
+		loses: "792"
+	},
+	{
+		id: "AE1AAC4B-8BDA-7161-48DD-9E2D515E25F6",
+		first_name: "Jane",
+		last_name: "Torres",
+		wins: "304",
+		loses: "390"
+	},
+	{
+		id: "4B2ADE4C-EDEB-8B94-B3AE-5E099CE1A7BD",
+		first_name: "Thor",
+		last_name: "Newton",
+		wins: "344",
+		loses: "252"
+	},
+	{
+		id: "8BF22328-76C8-BE8A-92F3-679D8D9D0235",
+		first_name: "Seth",
+		last_name: "Singleton",
+		wins: "454",
+		loses: "166"
+	},
+	{
+		id: "64C856D6-C70C-FA43-8431-E9693369D65D",
+		first_name: "Kylie",
+		last_name: "Fuentes",
+		wins: "613",
+		loses: "898"
+	},
+	{
+		id: "C757F912-6D56-5581-ECCC-87CC42C7DA77",
+		first_name: "Sylvia",
+		last_name: "Caldwell",
+		wins: "98",
+		loses: "219"
+	},
+	{
+		id: "F8B6B8D3-D936-A4D1-4963-A2D1CDDC08B8",
+		first_name: "Tanya",
+		last_name: "Pennington",
+		wins: "931",
+		loses: "959"
+	},
+	{
+		id: "E4CD33DB-35B6-081B-8782-46BF144F3652",
+		first_name: "Selma",
+		last_name: "Pace",
+		wins: "515",
+		loses: "219"
+	},
+	{
+		id: "7D1DEC6B-6BFC-B1D4-65EF-DA0078FBDE4E",
+		first_name: "Clayton",
+		last_name: "Mejia",
+		wins: "744",
+		loses: "513"
+	},
+	{
+		id: "C894D103-0212-A548-C0A9-22626C4AC66C",
+		first_name: "Gray",
+		last_name: "Hanson",
+		wins: "545",
+		loses: "646"
+	},
+	{
+		id: "EB9728B5-6440-E7D9-E2A8-3D9E3EA5160C",
+		first_name: "Caesar",
+		last_name: "Mcpherson",
+		wins: "333",
+		loses: "628"
+	},
+	{
+		id: "BEB5DEEA-4DAE-FADE-C30B-B6954E95984D",
+		first_name: "Lisandra",
+		last_name: "Swanson",
+		wins: "553",
+		loses: "510"
+	},
+	{
+		id: "8E1271F5-50A4-3F68-E359-444E9E4D3EC6",
+		first_name: "Ramona",
+		last_name: "George",
+		wins: "180",
+		loses: "355"
+	},
+	{
+		id: "6622EAC6-7BC7-1105-666A-2C7B04C42574",
+		first_name: "Ingrid",
+		last_name: "Lane",
+		wins: "143",
+		loses: "721"
+	},
+	{
+		id: "D595CCD5-46E0-6629-26AD-989C7167820C",
+		first_name: "Griffith",
+		last_name: "Ramsey",
+		wins: "893",
+		loses: "755"
+	},
+	{
+		id: "14793E78-E67F-1D96-B654-0B6ABAC380A3",
+		first_name: "Malcolm",
+		last_name: "Bentley",
+		wins: "538",
+		loses: "971"
+	},
+	{
+		id: "B37989AD-23A8-A180-6821-D91CB2428162",
+		first_name: "Sydnee",
+		last_name: "Coleman",
+		wins: "133",
+		loses: "723"
+	},
+	{
+		id: "83894B2B-6ACB-91A4-437A-4B64936CA8B7",
+		first_name: "Marvin",
+		last_name: "Estrada",
+		wins: "551",
+		loses: "614"
+	},
+	{
+		id: "1611A84B-4908-D53C-EA52-2EB2C75D5D29",
+		first_name: "Bernard",
+		last_name: "Hopper",
+		wins: "224",
+		loses: "806"
+	},
+	{
+		id: "9B39602D-4B29-6BDE-E956-4B4397941138",
+		first_name: "May",
+		last_name: "Nieves",
+		wins: "420",
+		loses: "699"
+	},
+	{
+		id: "EAB9C5BE-7E31-367D-FB04-0C8143DD4921",
+		first_name: "Fletcher",
+		last_name: "Dillard",
+		wins: "127",
+		loses: "53"
+	},
+	{
+		id: "3722C6C9-6CCD-24A3-A099-49AC789A2DD1",
+		first_name: "Lucian",
+		last_name: "Johnson",
+		wins: "12",
+		loses: "623"
+	}
+];
